16b6335266e638a4e526d18be77ed045bcb3c58d
hdrsy <hhdrgroup@gmail.com>
2024-01-04 11:51:42 +0300

    edit map section in event detailes to navigate into location screen

--- lib/features/events/event_detailes/view/widgets/inforamtion_event_widgets/location_section.dart
+++ lib/features/events/event_detailes/view/widgets/inforamtion_event_widgets/location_section.dart
@@ -8,64 +8,63 @@ import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 Widget buildLocationSection(BuildContext context) {
-  final EventDetailesController eventDetailesController=Get.find();
-  LatLng googleMapsCenter=LatLng(eventDetailesController.eventDetailsModel.venue.latitude, eventDetailesController.eventDetailsModel.venue.longitude);
+  final EventDetailesController eventDetailesController = Get.find();
+  LatLng googleMapsCenter = LatLng(
+      eventDetailesController.eventDetailsModel.venue.latitude,
+      eventDetailesController.eventDetailsModel.venue.longitude);
   final googleMapsController = Completer<GoogleMapController>();
-  return
-  
-        InkWell(
-        onTap: (){
-           Get.toNamed('/SeeLocation',arguments: [eventDetailesController.eventDetailsModel.venue.latitude,eventDetailesController.eventDetailsModel.venue.longitude]);
-        
-        },
-        child:  Column(
-    mainAxisSize: MainAxisSize.max,
-    children: [
-      Padding(
-        padding: const EdgeInsetsDirectional.fromSTEB(0, 20, 0, 8),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-               "Location" ,
-              style: customTextStyle.bodySmall.override(
-                fontFamily: 'Open Sans',
-                color: customColors.primaryText,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
+  return InkWell(
+      onTap: () {
+        Get.toNamed('/SeeLocation', arguments: [
+          eventDetailesController.eventDetailsModel.venue.latitude,
+          eventDetailesController.eventDetailsModel.venue.longitude
+        ]);
+      },
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(0, 20, 0, 8),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Location",
+                  style: customTextStyle.bodySmall.override(
+                    fontFamily: 'Open Sans',
+                    color: customColors.primaryText,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-Container(
-          width: double.infinity,
-          height: 200,
-          decoration: BoxDecoration(
-            color: customColors.secondaryBackground,
           ),
-          child: FlutterFlowGoogleMap(
-            controller: googleMapsController,
-            onCameraIdle: (latLng) =>
-                googleMapsCenter = latLng,
-            initialLocation: googleMapsCenter 
-                ,
-            markerColor: GoogleMarkerColor.violet,
-            mapType: MapType.normal,
-            style: GoogleMapStyle.standard,
-            initialZoom: 14,
-            allowInteraction: true,
-            allowZoom: true,
-            showZoomControls: true,
-            showLocation: true,
-            showCompass: false,
-            showMapToolbar: false,
-            showTraffic: false,
-            centerMapOnMarkerTap: true,
+          Container(
+            width: double.infinity,
+            height: 200,
+            decoration: BoxDecoration(
+              color: customColors.secondaryBackground,
+            ),
+            child: FlutterFlowGoogleMap(
+              controller: googleMapsController,
+              onCameraIdle: (latLng) => googleMapsCenter = latLng,
+              initialLocation: googleMapsCenter,
+              markerColor: GoogleMarkerColor.violet,
+              mapType: MapType.normal,
+              style: GoogleMapStyle.standard,
+              initialZoom: 14,
+              allowInteraction: false,
+              allowZoom: false,
+              showZoomControls: false,
+              showLocation: true,
+              showCompass: false,
+              showMapToolbar: false,
+              showTraffic: false,
+              centerMapOnMarkerTap: true,
+            ),
           ),
-        ),
-      
-    ],
-  ));
+        ],
+      ));
 }
